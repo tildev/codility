7006c0eece5f0e7ca806a21d354d467e0d93f454
tildev <tildevelove@gmail.com>
2020-12-30 20:12:06 +0900

    Add solution - the problem BinaryGap of lesson01

--- src/codility/lesson/l01/BinaryGap.java
+++ src/codility/lesson/l01/BinaryGap.java
@@ -7,6 +7,13 @@ package codility.lesson.l01;
  * @data 2020. 12. 30.
  */
 public class BinaryGap {
+
+    /**
+     * solution
+     *
+     * @param N
+     * @return
+     */
     public int solution(int N) {
         String binaryString = Integer.toBinaryString(N);
         int max = 0;
@@ -26,7 +33,39 @@ public class BinaryGap {
         return max;
     }
 
+    /**
+     * solution1
+     *
+     * @param N
+     * @return
+     */
+    public int solution1(int N) {
+        String binaryString = "";
+
+        while (N > 0) {
+            binaryString = (N % 2) + binaryString;
+            N /= 2;
+        }
+
+        int max = 0;
+        int impValue = 0;
+        for (int i = 1; i < binaryString.length(); i++) {
+
+            if (binaryString.charAt(i) - '0' == 0) {
+                impValue++;
+            } else {
+                if (max < impValue) {
+                    max = impValue;
+                }
+                impValue = 0;
+            }
+
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-        new BinaryGap().solution(1041);
+        System.out.println(new BinaryGap().solution(1041));
+        System.out.println(new BinaryGap().solution1(1041));
     }
 }
