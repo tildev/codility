91e1d7b5a3a6a041d68ff6fed1670bb2fb30fb31
tildev <tildevelove@gmail.com>
2020-12-31 22:00:56 +0900

    Solve the problem FrogRiverOne of lesson04

---
+++ src/codility/lesson/l04/FrogRiverOne.java
@@ -0,0 +1,39 @@
+package codility.lesson.l04;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Counting Elements - FrogRiverOne
+ * <p>
+ * Detected time complexity: O(N)
+ *
+ * @author tildev
+ * @data 2020. 12. 31.
+ */
+public class FrogRiverOne {
+
+    public int solution(int X, int[] A) {
+        Set<Integer> leaves  = new HashSet<>();
+        for (int i = 1; i <= X; i++) {
+            leaves.add(i);
+        }
+        int time = -1;
+        for (int i = 0; i < A.length; i++) {
+            if (leaves.contains(A[i])) {
+                leaves.remove(A[i]);
+            }
+            if (leaves.size() == 0) {
+                time = i;
+                break;
+            }
+        }
+        return time;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 3, 1, 4, 2, 3, 5, 4};
+        int X = 5;
+        System.out.println(new FrogRiverOne().solution(X, A));
+    }
+}
