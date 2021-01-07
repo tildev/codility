90f811ed1babb530c740d2283e838385161eb111
tildev <tildevelove@gmail.com>
2021-01-08 01:55:55 +0900

    Solve the problem StoneWall of l07

---
+++ src/codility/lesson/l07/StoneWall.java
@@ -0,0 +1,40 @@
+package codility.lesson.l07;
+
+import java.util.Stack;
+
+/**
+ * Stacks and Queues - StoneWall
+ * <p>
+ * Detected time complexity:
+ *
+ * @author tildev
+ * @data 2021. 01. 08.
+ */
+public class StoneWall {
+    public int solution(int[] H) {
+        Stack<Integer> stack = new Stack<>();
+        int blockCnt = 0;
+        for (int h : H) {
+            while (!stack.isEmpty()) {
+                if (stack.peek() > h) {
+                    stack.pop();
+                    blockCnt++;
+                } else if (stack.peek() < h) {
+                    stack.push(h);
+                    break;
+                } else {
+                    break;
+                }
+            }
+            if (stack.isEmpty()) {
+                stack.push(h);
+            }
+        }
+        return blockCnt + stack.size();
+    }
+
+    public static void main(String[] args) {
+        int[] H = {8, 8, 5, 7, 9, 8, 7, 4, 8};
+        System.out.println(new StoneWall().solution(H));
+    }
+}
