0adf8650b9a404b26a34d416d8e51f0c842a1409
tildev <tildevelove@gmail.com>
2021-02-23 21:43:14 +0900

    Solve the problem MaxNonoverlappingSegments of l16

---
+++ src/codility/lesson/l16/MaxNonoverlappingSegments.java
@@ -0,0 +1,32 @@
+package codility.lesson.l16;
+
+/**
+ * Greedy algorithms - MaxNonoverlappingSegments
+ * <p>
+ * Detected time complexity:
+ *
+ * @author tildev
+ * @data 2021. 02. 23.
+ */
+public class MaxNonoverlappingSegments {
+    public int solution(int[] A, int[] B) {
+        int result = 0;
+
+        int index = -1;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > index) {
+                result++;
+                index = B[i];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 3, 7, 9, 9};
+        int[] B = {5, 6, 8, 9, 10};
+        System.out.println(new MaxNonoverlappingSegments().solution(A, B));
+    }
+}
